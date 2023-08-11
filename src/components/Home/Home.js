6d032fc18b4b754dc283913594a2c9540f0337ff
Abelaash <abelaash.giritharan@gmail.com>
2023-08-11 09:39:48 -0400

    adding the translate layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,35 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 
+function App(){
+    return (
+        <div className="App">
+            <div>
+                Detect language:
+                <select>
+                    <option value="English">English</option>
+                    <option value="French">English</option>
+                    <option value="Tamil">Tamil</option>
+                    <option value="Bengali">Bengali</option>
+                    <option value="Vietnamese">Vietnamese</option>
+                </select>
+                Translate To:
+                <select>
+                    <option value="English">English</option>
+                    <option value="French">English</option>
+                    <option value="Tamil">Tamil</option>
+                    <option value="Bengali">Bengali</option>
+                    <option value="Vietnamese">Vietnamese</option>
+                </select>
+            </div>
+            <div>
+                <textarea cols="50" row="8"></textarea>
+            </div>
+
+            <div>
+                <textarea cols="50" row="8"></textarea>
+            </div>
+        </div>
+    )
+}
+export default App();
