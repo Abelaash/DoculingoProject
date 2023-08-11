c1bf9eac7794d597b4049d2723aab3fbd522de8d
Abelaash <abelaash.giritharan@gmail.com>
2023-08-11 10:41:29 -0400

    add half portion of App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
-import './App.css';
-import Home from './components/Home/Home';
+function App() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState('en');
+  const [from, setFrom] = useState('en');
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
 
-const App = () => {
-  <>
-    <Home/>
-  </>
+  const translate = () => {
+
+    const params = new URLSearchParams();
+    params.append('q', input);
+    params.append('source', from);
+    params.append('target', to);
+    params.append('api-key');
+  }
+
+  return (
+    <div className="App">
+      <div>
+        From ({from}) :
+        <select onChange={(e) => setFrom(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        To ({to}) :
+        <select onChange={(e) => setTo(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <textarea
+          cols="50"
+          rows="8"
+          onInput={(e) => setInput(e.target.value)}
+        ></textarea>
+      </div>
+      <div>
+        <textarea cols="50" rows="8" value={output}></textarea>
+      </div>
+      <div>
+        <button onClick={e=>translate()}>Translate</button>
+      </div>
+    </div>
+  );
 }
+
 export default App;
