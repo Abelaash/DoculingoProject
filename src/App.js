c3672c6d39b25397edf5e7dcd0ea5566c2d9b3ee
Abelaash <abelaash.giritharan@gmail.com>
2023-08-11 10:09:03 -0400

    delete Home component and Translate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+const App = () => {
+  <>
+    <Home/>
+  </>
 }
-
 export default App;
