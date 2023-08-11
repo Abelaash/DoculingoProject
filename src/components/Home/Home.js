cdaaf71328da3f371d43e94b16cc5eb4bcaeadc6
Abelaash <abelaash.giritharan@gmail.com>
2023-08-11 09:59:55 -0400

    added react-icons and imports

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,42 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Detect language:
+        <select>
+          <option value="English">English</option>
+          <option value="French">English</option>
+          <option value="Tamil">Tamil</option>
+          <option value="Bengali">Bengali</option>
+          <option value="Vietnamese">Vietnamese</option>
+        </select>
 
-function App(){
-    return (
-        <div className="App">
-            <div>
-                Detect language:
-                <select>
-                    <option value="English">English</option>
-                    <option value="French">English</option>
-                    <option value="Tamil">Tamil</option>
-                    <option value="Bengali">Bengali</option>
-                    <option value="Vietnamese">Vietnamese</option>
-                </select>
-                Translate To:
-                <select>
-                    <option value="English">English</option>
-                    <option value="French">English</option>
-                    <option value="Tamil">Tamil</option>
-                    <option value="Bengali">Bengali</option>
-                    <option value="Vietnamese">Vietnamese</option>
-                </select>
-            </div>
-            <div>
-                <textarea cols="50" row="8"></textarea>
-            </div>
+        Translate To:
+        <select>
+          <option value="English">English</option>
+          <option value="French">English</option>
+          <option value="Tamil">Tamil</option>
+          <option value="Bengali">Bengali</option>
+          <option value="Vietnamese">Vietnamese</option>
+        </select>
+      </div>
+      <div>
+        <textarea cols="50" row="8"></textarea>
+      </div>
 
-            <div>
-                <textarea cols="50" row="8"></textarea>
-            </div>
-            <div>
-                <button>Translate language</button>
-            </div>
-        </div>
-    )
+      <div>
+        <textarea cols="50" row="8"></textarea>
+      </div>
+      <div>
+        <button>
+          <AiOutlineArrowLeft /> Translate language <AiOutlineArrowRight />
+        </button>
+      </div>
+    </div>
+  );
 }
-export default App();
+
+export default App;
